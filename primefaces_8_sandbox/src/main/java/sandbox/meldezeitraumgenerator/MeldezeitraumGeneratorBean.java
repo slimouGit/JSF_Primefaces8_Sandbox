bf13c7f7d30b54b0c5b443eef95fafd4f52c3ec3
Salim Oussayfi <box@slimou.net>
2022-04-16 17:44:12 +0200

    toggle panel in MeldezeitraumGenerator

--- primefaces_8_sandbox/src/main/java/sandbox/meldezeitraumgenerator/MeldezeitraumGeneratorBean.java
+++ primefaces_8_sandbox/src/main/java/sandbox/meldezeitraumgenerator/MeldezeitraumGeneratorBean.java
@@ -1,5 +1,8 @@
 package sandbox.meldezeitraumgenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -7,14 +10,47 @@ import javax.inject.Named;
 import abstractbean.AbstractBean;
 
 @Named
-@ViewScoped
+@ViewScoped	
 public class MeldezeitraumGeneratorBean extends AbstractBean {
 	
 	private String title;
+	private String meldeZeitraumSuffix;
+	private List<String> meldezeitraumModusMap = new ArrayList<>();
+	private String meldeZeitraumPraefix;
+	private List<String> meldezeitraumPraefixListe = new ArrayList<>();
+	private boolean zeigeMeldezeitraumDetail_1;
+	private boolean zeigeMeldezeitraumDetail_2;
 	
 	@PostConstruct
 	public void init() {
 		this.title = "MeldezeitraumKomponente";
+		
+		this.meldezeitraumPraefixListe.add("Monat");
+		this.meldezeitraumPraefixListe.add("2 Monate");
+		this.meldezeitraumPraefixListe.add("Quartal");
+		this.meldezeitraumPraefixListe.add("Jahr");
+		
+		this.meldezeitraumModusMap.add("01");
+		this.meldezeitraumModusMap.add("02");
+		this.meldezeitraumModusMap.add("03");
+		this.meldezeitraumModusMap.add("04");
+		this.meldezeitraumModusMap.add("05");
+		this.meldezeitraumModusMap.add("06");
+		this.meldezeitraumModusMap.add("07");
+		this.meldezeitraumModusMap.add("08");
+		this.meldezeitraumModusMap.add("09");
+		this.meldezeitraumModusMap.add("10");
+		this.meldezeitraumModusMap.add("11");
+		this.meldezeitraumModusMap.add("12");
+	
+	}
+	
+	public boolean toggleMeldezeitraumDetail_1() {		
+		return this.zeigeMeldezeitraumDetail_1 ^= true;
+	}
+	
+	public boolean toggleMeldezeitraumDetail_2() {
+		return this.zeigeMeldezeitraumDetail_2 ^= true;
 	}
 	
 	public String getTitle() {
@@ -24,4 +60,59 @@ public class MeldezeitraumGeneratorBean extends AbstractBean {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+	public String getMeldeZeitraumSuffix() {
+		return meldeZeitraumSuffix;
+	}
+
+	public void setMeldeZeitraumSuffix(String meldeZeitraumSuffix) {
+		this.meldeZeitraumSuffix = meldeZeitraumSuffix;
+	}
+
+	public List<String> getMeldezeitraumModusMap() {
+		return meldezeitraumModusMap;
+	}
+
+	public void setMeldezeitraumModusMap(List<String> meldezeitraumModusMap) {
+		this.meldezeitraumModusMap = meldezeitraumModusMap;
+	}
+	
+	
+	public String getMeldeZeitraumPraefix() {
+		return meldeZeitraumPraefix;
+	}
+
+	public void setMeldeZeitraumPraefix(String meldeZeitraumPraefix) {
+		this.meldeZeitraumPraefix = meldeZeitraumPraefix;
+	}
+
+	public List<String> getMeldezeitraumPraefixListe() {
+		return meldezeitraumPraefixListe;
+	}
+
+	public void setMeldezeitraumPraefixListe(List<String> meldezeitraumPraefixListe) {
+		this.meldezeitraumPraefixListe = meldezeitraumPraefixListe;
+	}
+
+	public boolean isZeigeMeldezeitraumDetail_1() {
+		return zeigeMeldezeitraumDetail_1;
+	}
+
+	public void setZeigeMeldezeitraumDetail_1(boolean zeigeMeldezeitraumDetail_1) {
+		this.zeigeMeldezeitraumDetail_1 = zeigeMeldezeitraumDetail_1;
+	}
+
+	public boolean isZeigeMeldezeitraumDetail_2() {
+		return zeigeMeldezeitraumDetail_2;
+	}
+
+	public void setZeigeMeldezeitraumDetail_2(boolean zeigeMeldezeitraumDetail_2) {
+		this.zeigeMeldezeitraumDetail_2 = zeigeMeldezeitraumDetail_2;
+	}
+
+	
+	
+	
+	
+	
 }
